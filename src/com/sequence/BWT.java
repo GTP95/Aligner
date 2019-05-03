d536b9d26aea02fcc2b1df2d26cc2bececa71a4c
Giacomo <giacomo.tom.petrucci@tiscali.it>
2019-05-03 15:11:54 +0200

    Removed Maven and test because had problems to use junit inside intellij IDEA

--- src/com/sequence/BWT.java
+++ src/com/sequence/BWT.java
@@ -4,25 +4,26 @@ package com.sequence;
 import java.util.ArrayList;
 
 public class BWT {
-    private String string;
-    private String bwt;
     private ArrayList<String> table;
     private static char terminator='$';
 
-    public BWT(String string) {
-        this.string = string+terminator;
+    public BWT() {
         this.table=new ArrayList<String>();
     }
 
     public String bwt(String string){
         char temp;
+        string=string+terminator;
         String bwt=new String();
         for(int i=0;i<string.length();i++) {
-            table.add(this.string);
+            table.add(string);
             temp=string.charAt(string.length()-1); //get last character of the string
             string=string.substring(0, string.length()-1);  //removes the last character from the string
             string=temp+string;
         }
+
+        java.util.Collections.sort(table);
+
         for(int i=0;i<string.length();i++){
             bwt=table.get(i).charAt(string.length()-1)+bwt; //constructs the BWT taking the last column of table
         }
---
+++ src/com/sequence/HashMapMatcher.java
@@ -0,0 +1,8 @@
+package com.sequence;
+
+import java.util.ArrayList;
+
+public class HashMapMatcher {
+    private ArrayList<String> shortReads;
+    
+}
